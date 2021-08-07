6b4be5a40a7955c8dd6dd9b005109fd713e97e55
Nusrat8 <nusratkeyafb@gmail.com>
2021-08-08 02:08:10 +0600

    using ajax in products

---
+++ MVC/views/p_sugg.php
@@ -0,0 +1,13 @@
+<?php
+ include 'mainheader.php'; 
+ include 'adminheader.php';
+ include '../controllers/ProductController.php';
+ $key = $_GET["key"];
+ $products = search($key);
+ 
+ if(count($products)> 0){
+	 foreach($products as $p){
+		 echo "<p>".$p["name"]."</p>";
+	 }
+ }
+?>
